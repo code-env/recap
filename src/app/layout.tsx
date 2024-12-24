e13601a9de09e06ce5353c9f997711ba362b9d9b
code-env <org.codeen@gmail.com>
2024-12-24 11:14:23 +0100

    feat: just thinking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const eb_garamond = EB_Garamond({
 })
 
 export const metadata: Metadata = {
-  title: "jStack App",
+  title: "Recap",
   description: "Created using jStack",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 }
